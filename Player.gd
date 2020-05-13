5d971fb1124a28825dfb135adb6c2749fee7ee45
Glitch-art <56557289+Glitch-art@users.noreply.github.com>
2020-05-13 17:03:14 -0500

    Cambio de Carpeta "Navegacion"

--- Player.gd
+++ Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var MAX_SPEED = 500
 var ACCELERATION = 2000
 var motion = Vector2()
-
+	
 func _physics_process(delta):
 	var axis = get_input_axis()
 	if axis == Vector2.ZERO:
