25a7ce7a9165db45fe797d080099159caf85dcd0
Manuel0606 <macq09876@gmail.com>
2020-05-14 18:05:54 -0500

    Daño a Enemigo

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+var axis
+var player = load("res://Player.tscn")
+var player_instance
+var player_posicion
 #var velocidad = 150
 #var path = []
 #var navegacion = get_parent().get_parent().get_node("Navegacion")
@@ -26,3 +30,36 @@ extends KinematicBody2D
 
 func _ready():
 	pass # Replace with function body.
+
+func _physics_process(delta):
+	axis = position
+	player_instance = player.instance()
+	player_posicion = player_instance.position
+	animacion()
+	death()
+#	if(get_slide_collision(get_slide_count()-1) != null):
+#		var obj_col = get_slide_collision(get_slide_count()-1).collider
+#		if(obj_col.get_tree().get_nodes_in_group("Atack_Down")[0]):
+#			queue_free()
+
+func animacion():
+	if(axis.x < player_posicion.x ):
+		$AnimacionEnemy.flip_h = false
+		$AnimacionEnemy.animation = "Run"
+	if(axis.x > player_posicion.x):
+		$AnimacionEnemy.flip_h = true
+		$AnimacionEnemy.animation = "Run"
+	if(axis.length() > 0):
+		$AnimacionEnemy.play()
+		axis.normalized()
+	return axis.normalized()
+
+func death():
+	pass
+
+
+
+
+func _on_AreaEnemy_area_entered(area):
+	if(area.get_name() == "AtackDown"):
+		queue_free()
--- Player.gd
+++ Player.gd
@@ -3,8 +3,20 @@ extends KinematicBody2D
 var MAX_SPEED = 500
 var ACCELERATION = 2000
 var motion = Vector2()
-	
+var atackUpLoad = load("res://Arma/AtackUp.tscn")
+var atackUpInstance
+var atackDownLoad = load("res://Arma/AtackDown.tscn")
+var atackDownInstance
+var atackLeftLoad = load("res://Arma/AtackLeft.tscn")
+var atackLeftInstance
+var is_atack = false
+var atack_up = false
+var atack_down = false
+var atack_left = false
+
+
 func _physics_process(delta):
+	_atack()
 	var axis = get_input_axis()
 	if axis == Vector2.ZERO:
 		apply_friction(ACCELERATION * delta)
@@ -24,29 +36,71 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Tecla_Morir"):
 		get_tree().get_nodes_in_group("World")[0].death() # Funcion death (Morir)
 
+# Atacke
+func _atack():
+	if(Input.is_action_pressed("shoot_up") && !is_atack):
+		atackUpInstance = atackUpLoad.instance()
+		add_child(atackUpInstance)
+		is_atack = true
+		$Img.animation = "Atack_Up"
+		$Img.frame = 0
+		atack_up = true
+	
+	if(Input.is_action_pressed("shoot_down") && !is_atack):
+		atackDownInstance = atackDownLoad.instance()
+		add_child(atackDownInstance)
+		is_atack = true
+		$Img.animation = "Atack_Down"
+		$Img.frame = 0
+		atack_down = true
+	
+	if(Input.is_action_pressed("shoot_left") && !is_atack):
+		$Img.flip_h = false
+		atackLeftInstance = atackLeftLoad.instance()
+		atackLeftInstance.set_direction($Img.flip_h)
+		add_child(atackLeftInstance)
+		is_atack = true
+		$Img.animation = "Atack_Left"
+		$Img.frame = 0
+		atack_left = true
+	
+	if(Input.is_action_pressed("shoot_right") && !is_atack):
+		$Img.flip_h = true
+		atackLeftInstance = atackLeftLoad.instance()
+		atackLeftInstance.set_direction($Img.flip_h)
+		add_child(atackLeftInstance)
+		is_atack = true
+		$Img.animation = "Atack_Left"
+		$Img.frame = 0
+		atack_left = true
+
 # Movimiento
 func get_input_axis():
 	var axis = Vector2.ZERO
-	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	#axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	#axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	
-	if(axis.x > 0):
+	if(Input.is_action_pressed("ui_right") && !is_atack):
+		axis.x = 1
 		$Img.flip_h = true
 		$Img.animation = "Left"
-	if(axis.x < 0):
+	if(Input.is_action_pressed("ui_left") && !is_atack):
+		axis.x = -1
 		$Img.flip_h = false
 		$Img.animation = "Left"
 	
-	if(axis.y > 0):
+	if(Input.is_action_pressed("ui_down") && !is_atack):
+		axis.y = 1
 		$Img.animation = "Down"
-	if(axis.y < 0):
+	if(Input.is_action_pressed("ui_up") && !is_atack):
+		axis.y = -1
 		$Img.animation = "Up"
 		
 	if(axis.length() > 0):
 		$Img.play()
 		axis.normalized()
-	else:
-		$Img.stop()
+#	else:
+#		$Img.stop()
 	return axis.normalized()
 	
 
@@ -64,3 +118,17 @@ func apply_movement(acceleration):
 
 
 
+
+
+func _on_Img_animation_finished():
+	if(is_atack):
+		if(atack_up):
+			atackUpInstance.finish()
+			atack_up = false
+		if(atack_down):
+			atackDownInstance.finish()
+			atack_down = false
+		if(atack_left):
+			atackLeftInstance.finish()
+			atack_left = false
+		is_atack = false
